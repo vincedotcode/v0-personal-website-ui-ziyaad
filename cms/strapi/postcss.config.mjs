6ab702cd0d26d6a4283c2393786a4d09d9321d25
Vince Erkadoo <erkadoovince@gmail.com>
2025-11-21 15:14:47 +0400

    cms

---
+++ cms/strapi/postcss.config.mjs
@@ -0,0 +1,11 @@
+// strapi/postcss.config.mjs
+export default {
+    plugins: {
+      // If you want Tailwind in Strapi admin, do this:
+      // tailwindcss: {},
+      // autoprefixer: {},
+  
+      // If you don't care about custom PostCSS in admin, keep empty:
+    },
+  }
+  
